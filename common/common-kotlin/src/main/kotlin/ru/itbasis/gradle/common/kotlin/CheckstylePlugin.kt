b4b0959e09fff74f4963bc96f92953b190aa21f4
Victor Alenkov <victor@alenkov.name>
2020-05-31 14:49:16 +0300

    Added Jacoco reports refactoring

--- common/common-kotlin/src/main/kotlin/ru/itbasis/gradle/common/kotlin/CheckstylePlugin.kt
+++ common/common-kotlin/src/main/kotlin/ru/itbasis/gradle/common/kotlin/CheckstylePlugin.kt
@@ -2,45 +2,35 @@
 
 package ru.itbasis.gradle.common.kotlin
 
-import io.gitlab.arturbosch.detekt.DetektPlugin
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.invoke
-import org.gradle.kotlin.dsl.provideDelegate
-import org.gradle.kotlin.dsl.registering
 import org.gradle.language.base.plugins.LifecycleBasePlugin.CHECK_TASK_NAME
 import org.gradle.language.base.plugins.LifecycleBasePlugin.VERIFICATION_GROUP
+import ru.itbasis.gradle.common.kotlin.checkstyle.CheckstyleDetektAction
+import ru.itbasis.gradle.common.kotlin.checkstyle.CheckstyleJacocoAction
 
 class CheckstylePlugin : Plugin<Project> {
 	override fun apply(target: Project): Unit = target.run {
 		apply<BackendCommonPlugin>()
-		apply<DetektPlugin>()
 
-		configure<DetektExtension> {
-			config.setFrom(files(rootDir.resolve("config/detekt.yml")))
-			parallel = true
-			buildUponDefaultConfig = true
-			ignoreFailures = false
-			reports {
-				html { enabled = false }
-				txt { enabled = false }
-			}
-		}
 		tasks {
-			val checkstyle by registering {
+			register(CHECKSTYLE_TASK_NAME) {
 				group = VERIFICATION_GROUP
-
-				dependsOn("detekt")
 			}
 
 			named(CHECK_TASK_NAME) {
-				dependsOn(checkstyle)
+				dependsOn(CHECKSTYLE_TASK_NAME)
 			}
 		}
 
+		listOf(CheckstyleDetektAction(), CheckstyleJacocoAction()).forEach {
+			it.execute(target)
+		}
+	}
+
+	companion object {
+		const val CHECKSTYLE_TASK_NAME = "checkstyle"
 	}
 }
