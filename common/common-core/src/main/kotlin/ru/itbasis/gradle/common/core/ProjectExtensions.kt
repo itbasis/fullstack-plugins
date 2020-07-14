f8309c5255d28a17170ab0d34e8f6970eb832426
Victor Alenkov <victor@alenkov.name>
2020-07-14 09:24:43 +0300

    Unused code removed

--- common/common-core/src/main/kotlin/ru/itbasis/gradle/common/core/ProjectExtensions.kt
+++
@@ -1,6 +0,0 @@
-package ru.itbasis.gradle.common.core
-
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.extra
-
-fun Project.getExtraValueOrDefault(key: String, defaultValue: String) = if (extra.has(key)) extra[key].toString() else defaultValue
